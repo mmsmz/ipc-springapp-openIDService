1ba29925c51d8e1e29ec1dc2ace3bbfbc36ff748
mohamed <marafath07@gmail.com>
2020-11-24 19:30:00 +0530

    added spring initializer

---
+++ src/main/java/com/ipcopenIDService/OpenIdServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ipcopenIDService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenIdServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenIdServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ipcopenIDService/OpenIdServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipcopenIDService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenIdServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
